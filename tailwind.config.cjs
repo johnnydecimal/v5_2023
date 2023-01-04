ad3f4be9703b11ede0704fe55b9f14dadc6da2bb
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-04 17:22:33 +1100

    disable Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+// == DISABLED ==
 
-module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {},
-		fontFamily: {
-			mono: ["Berkeley Mono Trial", ...defaultTheme.fontFamily.mono],
-		},
-	},
-	plugins: [],
-};
+// const defaultTheme = require("tailwindcss/defaultTheme");
+//
+// module.exports = {
+// 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+// 	theme: {
+// 		extend: {},
+// 		fontFamily: {
+// 			mono: ["Berkeley Mono Trial", ...defaultTheme.fontFamily.mono],
+// 		},
+// 	},
+// 	plugins: [],
+// };
