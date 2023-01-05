afa1ff525b358185f0bff7bd8a87f6d0305839a0
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-05 16:58:54 +1100

    config: define ‘site’

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 import { defineConfig } from "astro/config";
 
-/**
-  == DISABLING TAILWIND ==
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	site: "https://johnnydecimal.com",
 });
-
-  */
