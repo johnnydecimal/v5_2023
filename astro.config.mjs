879bcab573b3cdc9e891ce4e3696b3c9214a4e1c
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-22 14:19:46 +1100

    tailwind: re-enable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-	site: "https://johnnydecimal.com",
-	integrations: [mdx()],
+  site: "https://johnnydecimal.com",
+  integrations: [mdx(), tailwind()]
 });
