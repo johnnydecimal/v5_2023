307a0ed2acc5150e89e1cdd495112f16cb214dbb
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-27 11:06:13 +1100

    📦 prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnnydecimal.com",
   // integrations: [mdx(), tailwind()]
-  integrations: [mdx()],
+  integrations: [mdx(), prefetch()],
 });
