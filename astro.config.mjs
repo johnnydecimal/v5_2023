6dd01be385759ccd9b872911d39025c7004c63f8
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-27 11:07:34 +1100

    Revert "📦 prefetch"
    This reverts commit 307a0ed2acc5150e89e1cdd495112f16cb214dbb.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnnydecimal.com",
   // integrations: [mdx(), tailwind()]
-  integrations: [mdx(), prefetch()],
+  integrations: [mdx()],
 });
