340bea512785a0ac7d439f32092b710549fa01d6
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-29 12:06:57 +1100

    📦 install & configure sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -9,5 +10,10 @@ import image from "@astrojs/image";
 export default defineConfig({
   site: "https://johnnydecimal.com",
   // integrations: [mdx(), tailwind()]
-  integrations: [mdx(), image()]
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
