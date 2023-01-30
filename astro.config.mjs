b7352881846ea4f1eecb6893588ec844b908d191
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-30 11:49:15 +1100

    add tailwind back!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-// import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
-// comment
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://johnnydecimal.com",
-  // integrations: [mdx(), tailwind()]
   integrations: [
     mdx(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    tailwind(),
   ],
 });
