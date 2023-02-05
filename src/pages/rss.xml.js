c07f9cb82f246b97ad56e2b6ecc3042bde613302
Johnny Noble <github-a3bd@johnny.net.au>
2023-02-06 10:21:06 +1100

    RSS!

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+
+export async function get(context) {
+  return rss({
+    // `<title>` field in output xml
+    title: "Johnny.Decimal - site feed",
+    // `<description>` field in output xml
+    description: "A system to organise projects.",
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: context.site,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: await pagesGlobToRssItems(
+      import.meta.glob("/src/pages/**/*.{md,mdx}")
+    ),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
