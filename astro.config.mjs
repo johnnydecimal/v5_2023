643e52a0275ba39dc81c16fa9b73dd87dd7277e7
Johnny Noble <github-a3bd@johnny.net.au>
2023-02-27 10:25:12 +1100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 // import tailwind from "@astrojs/tailwind";
 
+/**
+ * TODO: you'd like to set the `trailingSlash: 'always'` option so that local
+ *       build behaviour matches Netlify. You've had to hack around here and
+ *       there because it doesn't. But doing that breaks the building of
+ *       JDNavItem. So fix that later once the site is up.
+ */
+
 export default defineConfig({
   site: "https://johnnydecimal.com",
   integrations: [
